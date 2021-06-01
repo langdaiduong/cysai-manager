d4f5875ae203736bcfc22ad35295171f35fc013e
langdaiduong <38464506+langdaiduong@users.noreply.github.com>
2021-06-01 22:42:18 +0700

    fix

--- index.js
+++ index.js
@@ -40,7 +40,8 @@ app.get('/*', (req, res) => {
 });
 
 
-const port = 3000;
-app.listen(process.env.PORT || port) => {
-  console.log(`Server running on port ${port}`);
+var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
 });
