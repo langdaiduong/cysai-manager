651f3d4b22331dc6a7aa9e61569e35f98dede655
langdaiduong <38464506+langdaiduong@users.noreply.github.com>
2021-06-01 22:25:54 +0700

    update

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ initAPIs(app);
 // lien ket voi frontend
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.js'));
 });
 
 
